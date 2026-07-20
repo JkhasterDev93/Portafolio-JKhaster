ab8788023981e0231dac2b0e5495e05bb416f667
Khaster Dev <103134611+KhasterDev@users.noreply.github.com>
2026-07-19 18:37:18 -0600

    Update: Test Experiencia Responsive

--- src/Portafolio/Views/ModalForm.tsx
+++ src/Portafolio/Views/ModalForm.tsx
@@ -48,7 +48,7 @@ export const ModalForm = ({
 
   return (
     <div className="fixed inset-0 bg-[#464645]/50 flex justify-center items-center z-50">
-      <div className="w-5/6 max-w-7xl max-h-[90vh] p-4 rounded-[20px] bg-[#1C1C1B] overflow-y-auto relative">
+      <div className="w-120 h-180  md:w-5/6 md:max-w-7xl md:max-h-[90vh] p-4 rounded-[20px] bg-[#1C1C1B] overflow-y-auto no-scrollbar relative">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-white text-xl cursor-pointer"
@@ -56,41 +56,42 @@ export const ModalForm = ({
           ✕
         </button>
 
-        <div className="grid grid-cols-8 grid-rows-6 gap-4 h-[80vh] w-full">
-          <div className="col-span-3 row-span-6 rounded-3xl overflow-hidden">
-            <img
-              src={data.render[0].src}
-              alt={data.render[0].id}
-              className="w-full h-full object-cover"
-            />
-          </div>
+        <div className="flex flex-col md:grid md:grid-cols-8 md:grid-rows-6 md:h-[80vh] md:w-full">
 
-          <div className="col-span-5 row-span-4 col-start-4 text-white overflow-y-auto">
-            <h2 className="text-4xl font-Momo mb-6">
+          <div className="col-span-5 row-span-4 col-start-4 text-white overflow-y-auto no-scrollbar">
+            <h2 className="text-2xl md:text-4xl font-Momo mb-6 mt-10">
               {data.title}
             </h2>
 
             {data.pagraph.map((item, index) => (
               <div key={index} className="mb-5">
                 {item.subtitle && (
-                  <h3 className="text-3xl font-League mb-2">
+                  <h3 className="text-xl mb:text-3xl font-League mb-2">
                     {item.subtitle}
                   </h3>
                 )}
 
-                <p className="font-Darker text-xl">{item.text}</p>
+                <p className="font-Darker text-[15px] mb:text-xl">{item.text}</p>
               </div>
             ))}
           </div>
 
+          <div className="col-span-3 row-span-6 rounded-3xl overflow-y-auto no-scrollbar">
+            <img
+              src={data.render[0].src}
+              alt={data.render[0].id}
+              className="w-full h-60  md:w-full md:h-full object-cover"
+            />
+          </div>
+
           <div className="col-span-5 row-span-2 col-start-4 row-start-5 flex flex-col gap-4">
-            <span className="text-[#A4A497] font-Darker font-bold text-xl">Galería de imagenes...</span>
+            <span className="text-[#A4A497] font-Darker font-bold text-[14px] md:text-xl">Galería de imagenes...</span>
             <div className="flex gap-4 items-center">
                 {data.render.slice(1).map((img, index) => (
                 <div
                     key={img.id}
                     onClick={() => swapImages(index)}
-                    className="w-60 h-32 rounded-2xl overflow-hidden cursor-pointer transition hover:scale-105"
+                    className="w-25 h-15 md:w-60 md:h-32 rounded-2xl overflow-hidden cursor-pointer transition hover:scale-105"
                 >
                     <img
                     src={img.src}
